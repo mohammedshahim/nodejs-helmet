d18e12834fc9a893eef028308d4ef8c3a836b081
Shahim <muhdshahim786@gmail.com>
2023-06-20 12:33:44 +0400

    helmet

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const helmet = require("helmet");
+const axios = require("axios");
+
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+app.get("/", async function (req, res) {
+  try {
+    const data = await axios.get("https://api.publicapis.org/random");
+    //   send response 200
+    res.status(200).json(data.data);
+  } catch (error) {
+    res.status(error.response.status).json({ error: error.message });
+  }
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
